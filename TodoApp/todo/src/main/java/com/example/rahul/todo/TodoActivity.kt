f7c3f20aeeb1d386a64dd9aff9e081d558766b89
Rahul DMello <rvdmello96@gmail.com>
2019-07-14 18:31:03 -0400

    Add Todo module

---
+++ TodoApp/todo/src/main/java/com/example/rahul/todo/TodoActivity.kt
@@ -0,0 +1,12 @@
+package com.example.rahul.todo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TodoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_todo)
+    }
+}
