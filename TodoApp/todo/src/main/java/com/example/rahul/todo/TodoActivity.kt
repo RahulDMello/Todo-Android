05e76f5a4f46fc27d30eb33e3edb54bade51344f
Rahul DMello <rvdmello96@gmail.com>
2019-07-15 23:39:31 -0400

    Add glide and make a parallax scrollable image

--- TodoApp/todo/src/main/java/com/example/rahul/todo/TodoActivity.kt
+++ TodoApp/todo/src/main/java/com/example/rahul/todo/TodoActivity.kt
@@ -1,8 +1,10 @@
 package com.example.rahul.todo
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.bumptech.glide.Glide
 import com.example.rahul.todo.databinding.ActivityTodoBinding
 
 class TodoActivity : AppCompatActivity() {
@@ -10,5 +12,6 @@ class TodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = DataBindingUtil.setContentView<ActivityTodoBinding>(this, R.layout.activity_todo)
+        Glide.with(this).load(Uri.parse("https://img.freepik.com/free-photo/man-jumping-impossible-possible-cliff-sunset-background-business-concept-idea_1323-265.jpg?size=626&ext=jpg")).into(binding.image)
     }
 }
